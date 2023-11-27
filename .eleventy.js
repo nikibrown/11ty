ada9667b1e80aaf1acf92fdde1f048947d701e72
Niki Brown <hi@nikibrown.com>
2023-11-27 17:49:16 +0000

    add receipie, add listing

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
+
+    // eleventyConfig.addCollection("recipies", function(collection) {
+    //     return collection.getFilteredByGlob("recipies/**/*.njk");
+    // });
 };
